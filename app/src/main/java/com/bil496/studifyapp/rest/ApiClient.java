a5a760266f24f78f061e7ac35dd955af3149494b
Burak Uyar <burakuyar25@gmail.com>
2018-03-11 09:29:49 +0300

    Retrofit infrastructure created

---
+++ app/src/main/java/com/bil496/studifyapp/rest/ApiClient.java
@@ -0,0 +1,24 @@
+package com.bil496.studifyapp.rest;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by burak on 3/11/2018.
+ */
+
+public class ApiClient {
+    public static final String BASE_URL = "http://192.168.0.10:3000/";
+    private static Retrofit retrofit = null;
+
+
+    public static Retrofit getClient() {
+        if (retrofit==null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
---
+++ app/src/main/java/com/bil496/studifyapp/rest/ApiInterface.java
@@ -0,0 +1,28 @@
+package com.bil496.studifyapp.rest;
+
+import com.bil496.studifyapp.model.Place;
+import com.bil496.studifyapp.model.PlaceResponse;
+import com.bil496.studifyapp.model.User;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+/**
+ * Created by burak on 3/11/2018.
+ */
+
+public interface ApiInterface {
+    @GET("places?_sort=title")
+    Call<Place[]> getPlaces();
+
+    @GET("places/{place_id}/topics?_sort=user_count&_order=desc&_embed=subtopics")
+    Call<String> getTopics(@Path("place_id") long placeId);
+
+    @GET("topics/{topic_id}/teams?_embed=users&sort=utility_score&order=desc")
+    Call<String> getTeams(@Path("topic_id") long topicId);
+
+    @GET("users")
+    Call<User[]> getUser(@Query("username") String username);
+}
