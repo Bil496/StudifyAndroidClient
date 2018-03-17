08a6af2927d272406f6897a8977058cd4db44006
Burak Uyar <burakuyar25@gmail.com>
2018-03-17 17:35:55 +0300

    Notification library and observer library have been added

--- app/src/main/java/com/bil496/studifyapp/service/MyFirebaseMessagingService.java
+++ app/src/main/java/com/bil496/studifyapp/service/MyFirebaseMessagingService.java
@@ -1,9 +1,15 @@
 package com.bil496.studifyapp.service;
 
+import android.app.Notification;
+import android.os.Bundle;
 import android.util.Log;
 
+import com.bil496.studifyapp.IntroActivity;
+import com.bil496.studifyapp.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import br.com.goncalves.pugnotification.notification.PugNotification;
 
 import static android.content.ContentValues.TAG;
 
@@ -23,6 +29,16 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            PugNotification.with(this)
+                    .load()
+                    .title(remoteMessage.getNotification().getTitle())
+                    .message(remoteMessage.getNotification().getBody())
+                    .bigTextStyle(remoteMessage.getNotification().getBody())
+                    .smallIcon(R.drawable.ic_launcher)
+                    .largeIcon(R.mipmap.ic_launcher)
+                    .flags(Notification.DEFAULT_ALL)
+                    .simple()
+                    .build();
         }
 
         // Also if you intend on generating your own notifications as a result of a received FCM
