59862aee7d2b702d5c1f55c5cdbd902a958bc379
Ahmet Selim Kaya <askaya@etu.edu.tr>
2018-03-24 01:21:38 +0300

    TopicAdapter class updated according to Filterable interface.

--- app/src/main/java/com/bil496/studifyapp/adapter/TopicAdapter.java
+++ app/src/main/java/com/bil496/studifyapp/adapter/TopicAdapter.java
@@ -5,6 +5,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -12,6 +14,7 @@ import com.app.infideap.stylishwidget.util.TextViewUtils;
 import com.bil496.studifyapp.R;
 import com.bil496.studifyapp.model.Topic;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -21,11 +24,47 @@ import butterknife.ButterKnife;
  * Created by burak on 3/11/2018.
  */
 
-public class TopicAdapter extends RecyclerView.Adapter<TopicAdapter.TopicViewHolder>{
+public class TopicAdapter extends RecyclerView.Adapter<TopicAdapter.TopicViewHolder> implements Filterable{
     private List<Topic> topics;
+    private List<Topic> filteredTopics;
     private int rowLayout;
     private Context context;
     CustomItemClickListener listener;
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String charString = charSequence.toString();
+                if (charString.isEmpty()) {
+                    filteredTopics = topics;
+                } else {
+                    List<Topic> filteredList = new ArrayList<>();
+                    for (Topic row : filteredList) {
+
+                        // name match condition. this might differ depending on your requirement
+                        // here we are looking for name or phone number match
+                        if (row.getTitle().toLowerCase().contains(charString.toLowerCase())) {
+                            filteredList.add(row);
+                        }
+                    }
+
+                    filteredTopics = filteredList;
+                }
+
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = filteredTopics;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                filteredTopics = (ArrayList<Topic>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
 
     public static class TopicViewHolder extends RecyclerView.ViewHolder {
         @BindView(R.id.title) TextView title;
@@ -42,6 +81,7 @@ public class TopicAdapter extends RecyclerView.Adapter<TopicAdapter.TopicViewHol
         this.rowLayout = rowLayout;
         this.context = context;
         this.listener = listener;
+        this.filteredTopics = topics;
     }
 
     @Override
@@ -70,6 +110,6 @@ public class TopicAdapter extends RecyclerView.Adapter<TopicAdapter.TopicViewHol
 
     @Override
     public int getItemCount() {
-        return topics.size();
+        return filteredTopics.size();
     }
 }
